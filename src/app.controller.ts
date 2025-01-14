81ce3e23437c17a78f26c4bb5a93d3da9a058009
SCO-Techlab <sco.techlab@gmail.com>
2025-01-14 19:04:39 +0100

    add example v1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,43 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Param, Post, UploadedFile, UseInterceptors } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { GridfsFile, GridfsGetFileOptions, GridfsService } from "@app/nestjs-gridfs-mongodb";
 
 @Controller('nestjs-gridfs-mongodb')
 export class AppController {
 
-  constructor() {}
+  constructor(private readonly gridfsService: GridfsService) {}
 
-  @Get()
-  async dummy() {
-    return 'Hello world - Nestjs Gridfs Mongodb!';
+  @Post('uploadFiles/:bucketName')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFiles(
+    @UploadedFile() file: Express.Multer.File,
+    @Param('bucketName') bucketName: string,
+  ): Promise<boolean> {
+    // You can expect a single file (File) or an array of files (File[])
+    return await this.gridfsService.uploadFiles(bucketName, file);
+  }
+  
+  @Post('getFiles/:bucketName')
+  async getFiles(
+    @Param('bucketName') bucketName: string,
+    @Body() options: GridfsGetFileOptions,
+  ): Promise<GridfsFile> {
+    /* 
+      You provide options to manage query filter.
+      Options is an object with the following properties:
+      - filter: Object (Object with the properties to filter the documents, same work as moongose find filter)
+      - includeBuffer: boolean (Will return the GridfsFile object with the buffer property, buffer includes the data and base64 of the file)
+      - single: boolean (Will return a single document (GridfsFile) or an array of documents (GridfsFile[]))
+    */
+    return await this.gridfsService.getFiles(bucketName, options) as GridfsFile;
+  }
+
+  @Post('deleteFiles/:bucketName')
+  async deleteFiles(
+    @Param('bucketName') bucketName: string,
+    @Body() data: any,
+  ): Promise<boolean> {
+    // You can expect a single id (string) or an array of ids (string[])
+    return await this.gridfsService.deleteFiles(bucketName, data._ids ?? []);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GridfsModule } from '@app/nestjs-gridfs-mongodb';
  
 @Module({
   imports: [
-    
+    MongooseModule.forRoot('mongodb://localhost:27017/nestjs-gridfs-mongodb'), 
+
+    // Register GridfsModule exports GridfsService in global mode
+    GridfsModule.register(),
   ],
   controllers: [
     AppController,
   ],
-  providers: [],
+  providers: [
+    AppService,
+  ],
 })
 
 export class AppModule {}
